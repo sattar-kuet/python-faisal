34bee4db6fdbf3f7c40deb353854ee7097285c44
arfin <arfinchowdhuryarif7@gamil.com>
2022-09-22 21:29:32 +0600

    push update

---
+++ dynamic_data_type.py
@@ -0,0 +1,3 @@
+product_code = 10225
+product_code = "10225"
+print(type(product_code))
---
+++ variable.py
@@ -0,0 +1,20 @@
+number = 15
+print(number)
+
+number = 10
+
+number2 = number*2
+
+number3 = number+number2
+number3 = number-number2
+number4 = number*number2
+number5 = number/number2
+number6 = number%3
+x= 10
+y = 10 - 10*x
+y1 = (10 - 10)*x
+print(y)
+print(y1)
+
+
+
